78b0d9a3943cdd4e183c776b4bf4fe09fb964840
Timo Betcke <timo.betcke@gmail.com>
2009-09-07 15:30:38 +0100

    Added constant basis function, working on quadrature.

--- libs/bem2d.h
+++ libs/bem2d.h
@@ -1,7 +1,16 @@
 #ifndef _BEM2D_H
 #define	_BEM2D_H
 
+#include "exceptions.h"
+#include "quadrature.h"
+
+
+#include "bem2ddefs.h"
 #include "Point.h"
 #include "Element.h"
+#include "ConstBasis.h"
+
+
 
 #endif // _BEM2D_H
+
