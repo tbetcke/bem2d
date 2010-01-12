7f627610068c610b5daa15875a4c8d4eda1a87dc
Timo Betcke <t.betcke@reading.ac.uk>
2010-01-12 11:09:23 +0000

    Small changes in disknormcond.cpp

--- normcomps/disknormcond.cpp
+++ normcomps/disknormcond.cpp
@@ -3,12 +3,14 @@
 #include<cmath>
 #include<ctime>
 #include<vector>
+#include<fstream>
 
 int main(int argc, char** argv)
 {
 
-  int ppw=40;     // Point per wavelength
-
+  int ppw=10;     // Point per wavelength
+  std::string filename="disknormcond10.txt";
+  
  
   std::vector<bem2d::freqtype> freqs;
   freqs.push_back(5);
@@ -17,10 +19,12 @@ int main(int argc, char** argv)
 
   std::vector<double> norm_sl(freqs.size());
   std::vector<double> norm_dl(freqs.size());
-  std::vector<double> norm_combined(freqs.size());
+  std::vector<double> norm_combined1(freqs.size());
+  std::vector<double> norm_combined2(freqs.size());
   std::vector<double> cond_sl(freqs.size());
   std::vector<double> cond_dl(freqs.size());
-  std::vector<double> cond_combined(freqs.size());
+  std::vector<double> cond_combined1(freqs.size());
+  std::vector<double> cond_combined2(freqs.size());
 
 
 
@@ -33,8 +37,8 @@ int main(int argc, char** argv)
         MPI_Init(&argc, &argv);
 
 
-        int nprow=2; // Number of rows in process grid
-        int npcol=1; // Number of columns in process grid
+        int nprow=4; // Number of rows in process grid
+        int npcol=2; // Number of columns in process grid
         int mb=100;  // Row Block size
         int nb=100;  // Column Block size
         bem2d::BlacsSystem* b=bem2d::BlacsSystem::Initialize(nprow,npcol,mb,nb);
@@ -55,7 +59,8 @@ int main(int argc, char** argv)
 	for (int j=0;j<freqs.size();j++){
 
 	double k=(double)freqs[j];
-	double eta=k; // Coupling between conj. double and single layer pot.
+	double eta1=k; // Coupling between conj. double and single layer pot.
+	double eta2=cbrt(k*k);
         bem2d::pCurve cobj(new bem2d::Circle);
 	int n=(int)(cobj->Length()*k*ppw/2/bem2d::PI);
         bem2d::AnalyticCurve circle(n,cobj);
@@ -90,12 +95,14 @@ int main(int argc, char** argv)
 	bem2d::Matrix ddl=(*DiscreteKernel(*pgeom,quadopts,dl));
 	bem2d::Matrix dcdl=*(DiscreteKernel(*pgeom,quadopts,cdl));
 	bem2d::Matrix Id=*(EvalIdent(*pgeom, quadopts));
-	bem2d::Matrix combined=Id+2.0*dcdl-bem2d::complex(0,2.0)*eta*dsl;
+	bem2d::Matrix combined1=Id+2.0*dcdl-bem2d::complex(0,2.0)*eta1*dsl;
+	bem2d::Matrix combined2=Id+2.0*dcdl-bem2d::complex(0,2.0)*eta2*dsl;
 	
 	dsl=2.0*bem2d::ChangeBasis(dsl,Id);
 	ddl=2.0*bem2d::ChangeBasis(ddl,Id);
 	dcdl=2.0*bem2d::ChangeBasis(dcdl,Id);
-	combined=bem2d::ChangeBasis(combined,Id);
+	combined1=bem2d::ChangeBasis(combined1,Id);
+	combined2=bem2d::ChangeBasis(combined2,Id);
 
 #ifdef BEM2DMPI
 	if (b->IsRoot()){
@@ -108,7 +115,8 @@ int main(int argc, char** argv)
 
 	bem2d::L2NormCond(dsl,norm_sl[j],cond_sl[j]);
 	bem2d::L2NormCond(ddl,norm_dl[j],cond_dl[j]);
-	bem2d::L2NormCond(combined,norm_combined[j],cond_combined[j]);
+	bem2d::L2NormCond(combined1,norm_combined1[j],cond_combined1[j]);
+	bem2d::L2NormCond(combined2,norm_combined2[j],cond_combined2[j]);
 	
 	}
         finish=clock();
@@ -119,25 +127,37 @@ int main(int argc, char** argv)
 	if (b->IsRoot()){
 #endif
 
- 	  std::cout << "Single Layer" << std::endl;
+	  std::ofstream out(filename.c_str());
+ 	  out << "Single Layer" << std::endl;
 
 	  for (int j=0;j<freqs.size();j++){
-	    std::cout << "k=" << freqs[j] << " Norm: " << norm_sl[j] << " Norm of Inverse: " << cond_sl[j]/norm_sl[j] << " Condition Nr.: " << cond_sl[j] <<  std::endl;
+	    out << "k=" << freqs[j] << " Norm: " << norm_sl[j] << " Norm of Inverse: " << cond_sl[j]/norm_sl[j] << " Condition Nr.: " << cond_sl[j] <<  std::endl;
 	  }
 
- 	  std::cout << "Double Layer" << std::endl;
+ 	  out << "Double Layer" << std::endl;
+	  
 
 	  for (int j=0;j<freqs.size();j++){
-	    std::cout << "k=" << freqs[j] << " Norm: " << norm_dl[j] << " Norm of Inverse: " << cond_dl[j]/norm_dl[j] << " Condition Nr.: " << cond_dl[j] <<  std::endl;
+	    out << "k=" << freqs[j] << " Norm: " << norm_dl[j] << " Norm of Inverse: " << cond_dl[j]/norm_dl[j] << " Condition Nr.: " << cond_dl[j] <<  std::endl;
 	  }
 
- 	  std::cout << "Combined Layer" << std::endl;
+ 	  out << "Combined Layer eta=k" << std::endl;
 
 	  for (int j=0;j<freqs.size();j++){
-	    std::cout << "k=" << freqs[j] << " Norm: " << norm_combined[j] << " Norm of Inverse: " << cond_combined[j]/norm_combined[j] << " Condition Nr.: " << cond_combined[j] <<  std::endl;
+	    out << "k=" << freqs[j] << " Norm: " << norm_combined1[j] << " Norm of Inverse: " << cond_combined1[j]/norm_combined1[j] << " Condition Nr.: " << cond_combined1[j] <<  std::endl;
 	  }
 
-	  std::cout << "Overalll time (minutes): " << time << std::endl;
+ 	  out << "Combined Layer eta=k^(2/3)" << std::endl;
+
+	  for (int j=0;j<freqs.size();j++){
+	    out << "k=" << freqs[j] << " Norm: " << norm_combined2[j] << " Norm of Inverse: " << cond_combined2[j]/norm_combined2[j] << " Condition Nr.: " << cond_combined2[j] <<  std::endl;
+	  }
+
+
+
+	  out << "Overalll time (minutes): " << time << std::endl;
+	  std::cout << "Overall time (minutes): " << time << std::endl;
+	  out.close();
 #ifdef BEM2DMPI	
 }
 #endif
